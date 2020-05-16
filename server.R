a0485a2e5816494f1d4305b55e88ba208373e671
lengghd <59861153+lengghd@users.noreply.github.com>
2020-05-16 09:22:29 +0800

    Update server.R

--- server.R
+++ server.R
@@ -31,24 +31,27 @@ shinyServer(function(input, output, session) {
     return(ts_data)
   })
   
-  getDateFromInput <- reactive({
+  #date
+  getDateFromInput <- reactive({                       #reactive: dataInput(): input$dateCol: pull(data, dateCol)
     data <- dataInput()
     dateCol <- input$dateCol
     date <- pull(data, dateCol)
     return(date)
   })
   
-  getDataFromInput <- reactive({
-    data <- dataInput()
-    dataCol <- input$dataCol
-    data <- pull(data, dataCol)
+  
+  #data
+  getDataFromInput <- reactive({                       #reactive
+    data <- dataInput()                                #dataInput()
+    dataCol <- input$dataCol                           #input$dataCol
+    data <- pull(data, dataCol)                        #pull(data, dataCol)
     return(data)
   })
   #
-  output$dataTable <- DT::renderDataTable({
-    date <- getDateFromInput()
-    data <- getDataFromInput()
-    return(data.frame(date, data))
+  output$dataTable <- DT::renderDataTable({           #DT::renderDataTable({
+    date <- getDateFromInput()                         # getDateFromInput()
+    data <- getDataFromInput()                         # getDataFromInput()
+    return(data.frame(date, data))                     #data.frame(date, data)
   })
 #  renderDT(df, options = list(
 #    pageLength = 5,
@@ -58,13 +61,44 @@ shinyServer(function(input, output, session) {
   
   #renderDT: datatable(DF) : formatStyle : X = styleInterval(#, c('col1', 'col2')
   
-  observeEvent(dataInput(), {
-    updateSelectInput(session, "dataCol", choices = names(dataInput()))
+  #observe和reactive之间的区别。
+  #一个打算在某个反应变量“触发”时运行，并具有副作用（observeEvent），
+  eventReactive创建的对象yo的定义与reactive相同，
+  
+  。但是，它像其他reactives一样被延迟评估和缓存。
+
+
+
+因此，如果您需要数据框，向量，图或其他东西，但又想与通常的反应链反应脱钩，请使用eventReactive。
+
+如果您只是想立即造成副作用，请使用observeEvent。
+  
+  #另一个则返回反应值，并用作变量（eventReactive）。
+  # #eventReactive创建一个基于eventExpr而变化的反应值，
+  #ventReactive没有通常的连锁反应行为, can get from rective
+  
+  # observeEvent则仅基于eventExpr被触发
+  #即使在这些功能的文档中，前者也没有分配给变量（因为它只是产生副作用）而已显示，而后者也被分配给了变量并在以后使用。
+  #observeEvent无法创建您定义的对象（它将创建其他对象）。立即评估并且不对其进行缓存。它用于引起副作用。
+  
+  #observeEvent returns an observer reference class object
+  observeEvent(
+    dataInput(), 
+    {   # change the value of a select input on the client
+      updateSelectInput(session,
+                        "dataCol",
+                        choices = names(dataInput()))
   })
   
-  observeEvent(dataInput(), {
-    updateSelectInput(session, "dateCol", choices = names(dataInput()))
+  observeEvent(
+    dataInput(), 
+    {
+    updateSelectInput(session, 
+                      "dateCol", 
+                      choices = names(dataInput()))
   })
+  
+  
   
   make_ts <- reactive({
     data <- getDataFromInput()
@@ -106,3 +140,6 @@ shinyServer(function(input, output, session) {
   })
 })
 
+#resets variables further down the chain, 
+#nd doesn't respond to user inputs unless the value has changed. 
+#his avoids duplicate or missing events.
